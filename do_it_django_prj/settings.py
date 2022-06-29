6db84459772d9d499e50696252af3d3a876bb82b
SEONGHYUN KOO <49744571+qkoo0833@users.noreply.github.com>
2022-06-29 23:20:14 +0900

    update security

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x-n5juykc)8)7mz4bhp^fzyx0nthg8i29a+@lg216u+7*7-okk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
